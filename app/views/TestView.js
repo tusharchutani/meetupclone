801ee668830869312b38aa72f90ea46a8d18518e
Tushar Chutani <tushar_chutani@yahoo.com>
2017-08-23 11:55:26 -0700

    Added map movement and eventInfo window

--- app/views/TestView.js
+++ app/views/TestView.js
@@ -5,45 +5,105 @@ import {
   Text,
   View
 } from 'react-native';
+import { MapView } from 'expo';
+
+
+import Constants from '../MokUI/UIConstants';
+import Carousel from 'react-native-snap-carousel';
+import EventFeedItem from './EventFeed/EventFeedItem';
 
 export default class TestView extends Component {
+
+  _mapView = null;
+  
   constructor(props, context) {
      super(props, context);
     this.state = {
       navigation:{
        screenName:'NA'
+      },
+    }
+    //TODO: get stuff from rest API
+    this.state = {
+      entries:[
+        {title:"title 1"},
+        {title:"title 2"},
+        {title:"title 3"},
+        {title:"title 4"},
+        {title:"title 5"},
+        {title:"title 6"}
+      ],
+      region: { 
+        latitude: 37.78825, 
+        longitude: -122.4324, 
+        latitudeDelta: 0.0922, 
+        longitudeDelta: 0.0421
       }
     }
   }
-  render() {
-    const {state} = this.props.navigation;
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          test
-        </Text>
-      </View>
-    );
-  }
-}
 
+
+    _centerMapOnMarker (markerIndex) {
+        const mapRef = this._mapView;
+
+        mapRef.animateToRegion({region:{
+                    latitude: 137.78825,
+                    longitude: -22.4324,
+                    latitudeDelta: 0.0315,
+                    longitudeDelta: 0.0258
+                }});
+    }
+
+  render () {
+      const slides = this.state.entries.map((entry, index) => {
+          return (
+                  <View key={`entry-${index}`} style={styles.slide}>
+                      <EventFeedItem />
+                  </View>
+          );
+      });
+
+      return (
+
+        <View style={styles.container}>
+        
+          <MapView ref={(mapView) => { this._mapView = mapView; }} style={styles.container} initialRegion={this.state.region}/>
+           <Carousel
+                  firstItem={2}
+                  inactiveSlideScale={0.94}
+                  inactiveSlideOpacity={0.6}              
+                  ref={(carousel) => { this._carousel = carousel; }}
+                  containerCustomStyle={styles.sliderContainer}
+                  sliderWidth={Constants.screenWidth}
+                  itemWidth={sliderWidth}
+                  onSnapToItem={(index, marker) => this._centerMapOnMarker(index)}                
+                >
+                    { slides }
+              </Carousel>
+           </View>
+
+          
+      );
+  }
+
+
+}
+const sliderWidth = 200;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'green',
+    justifyContent: 'flex-end'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  slide:{
+    height:150, 
+    width:sliderWidth
+  },sliderContainer:{
+    position:'absolute',
+    flex:1,
+    backgroundColor:'transparent',
+    paddingBottom: 50
+  }
 });
+
 
 AppRegistry.registerComponent('TestView', () => TestView);
